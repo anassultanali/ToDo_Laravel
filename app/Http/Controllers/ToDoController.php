efcc918b3739a61f550732f2bd5116aa7305151e
anassultan <anassultanali@gmail.com>
2025-03-26 11:50:52 +0300

    store and feach the data

---
+++ app/Http/Controllers/ToDoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ToDo;
+
+class ToDoController extends Controller
+{
+    function index() {
+        
+        $tasks = ToDo::all();
+        
+        return view('welcome' , ['tasks' => $tasks]);
+    }
+    
+    function store() {
+        //get data
+        $title = request()->title;
+        $description = request()->description;
+
+        //send the data to DB
+        ToDo::create([
+            'title' => $title,
+            'description' => $description
+        ]);
+
+        return redirect('/' );
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ToDoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/' , [ToDoController::class , 'index']);
+Route::post('/' ,[ToDoController::class , 'store'] );
